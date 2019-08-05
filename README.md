a1c727aca94da687bbec3ed0476e0614f79cdd5a
Mayank Aggarwal <maggarwal7@sapient.com>
2019-08-05 20:40:28 +0100

    Added Instance controller

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # gcloud-app
+
+mvn clean package
+
+mvn appengine:run
+
+mvn appengine:deploy
+
+curl --header "project-id: my-kubernetes-248208" --header "zone: europe-west2-a" http://localhost:8080/instances
+
+
--- src/main/java/com/ma/programming/app/AppApplication.java
+++ src/main/java/com/ma/programming/app/AppApplication.java
@@ -2,12 +2,27 @@ package com.ma.programming.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class AppApplication {
+@RestController
+public class AppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AppApplication.class);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "hello world! - from SpringBoot app";
 	}
 
 }
